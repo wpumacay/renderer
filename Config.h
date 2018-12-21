a450e29d5544a3483c4fc52a8178ab2a63430cab
wpumacay <wpumacay@gmail.com>
2018-12-21 01:50:52 -0500

    [UPDATE]: just modified window name

--- Config.h
+++ Config.h
@@ -4,7 +4,7 @@
 #define APP_CONTEXT_VERSION_MAJOR 3
 #define APP_CONTEXT_VERSION_MINOR 3
 
-#define APP_NAME        "Visualizer"
+#define APP_NAME        "Custom Visualizer"
 #define APP_RESIZABLE   0
 
 #define APP_WIDTH  1024
