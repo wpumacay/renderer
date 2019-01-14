705a8564cc8f51b3ec500cf123a44981338d2c34
wpumacay <wpumacay@gmail.com>
2019-01-13 22:44:45 -0500

    [MAINTENANCE]: changing examples structure

--- examples/sample2_meshes.cpp
+++ examples/sample2_meshes.cpp
@@ -7,9 +7,6 @@
 #include <LLightDirectional.h>
 #include <LMeshBuilder.h>
 
-#ifndef ENGINE_RESOURCES_PATH
-    #define ENGINE_RESOURCES_PATH "../res"
-#endif
 
 int main()
 {
@@ -17,11 +14,13 @@ int main()
     auto _scene = _app->scene();
     
     // make a sample mesh just for testing
-    std::string _modelpath;
-    _modelpath += ENGINE_RESOURCES_PATH;
-    _modelpath += "/models/chassis.stl";
+    // auto _mesh = engine::LMeshBuilder::createBox( 0.5f, 0.5f, 0.5f );
+    // auto _mesh = engine::LMeshBuilder::createArrow( 1.0f, "x" );
+    // auto _mesh = engine::LMeshBuilder::createArrow( 1.0f, "y" );
+    // auto _mesh = engine::LMeshBuilder::createArrow( 1.0f, "z" );
+    // auto _mesh = engine::LMeshBuilder::createSphere( 0.2f );
+    auto _mesh = engine::LMeshBuilder::createAxes( 1.0f );
 
-    auto _model = engine::LMeshBuilder::createModelFromFile( _modelpath, "chassis" );
     // make a sample camera
     auto _camera = new engine::LFpsCamera( "fixed",
                                            engine::LVec3( 1.0f, 2.0f, -1.0f ),
@@ -32,6 +31,7 @@ int main()
     //                                            engine::LVec3( 1.0f, 2.0f, 1.0f ),
     //                                            engine::LVec3( 0.0f, 0.0f, 0.0f ),
     //                                            engine::LICamera::UP_Z );
+    
     // make a sample light source
     auto _light = new engine::LLightDirectional( engine::LVec3( 0.2, 0.2, 0.2 ), engine::LVec3( 0.8, 0.8, 0.8 ),
                                                  engine::LVec3( 0.05, 0.05, 0.05 ), 0, engine::LVec3( -1, -1, 0 ) );
@@ -39,7 +39,9 @@ int main()
     // add these components to the scene
     _scene->addCamera( _camera );
     _scene->addLight( _light );
-    _scene->addRenderable( _model );
+    _scene->addRenderable( _mesh );
+
+    float _t = 0.0;
 
     while( _app->isActive() )
     {
@@ -47,9 +49,14 @@ int main()
         engine::DebugSystem::drawLine( { 0.0f, 0.0f, 0.0f }, { 0.0f, 5.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } );
         engine::DebugSystem::drawLine( { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 5.0f }, { 0.0f, 0.0f, 1.0f } );
 
-        _app->update();
+        _mesh->rotation = _mesh->rotation * engine::LMat4::rotationZ( 0.01f );
+        _mesh->pos.x = std::sin( _t );
 
-        _model->rotation = _model->rotation * engine::LMat4::rotationX( 0.01f );
+        _t += 0.01f;
+
+        _app->begin();
+        _app->update();
+        _app->end();
     }
 
     return 0;
