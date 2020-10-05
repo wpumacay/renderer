f38043e3576255817125fdd14229eea02d4dfbc9
Wilbert <wpumacay@gmail.com>
2020-10-04 23:04:43 -0500

    [Maintenance]: move old bindings to _legacy

--- python/tinyrenderer/core_egl.py
+++
@@ -1,7 +0,0 @@
-
-engine = None
-try :
-    import tinyrenderer_core_egl as tr_core_egl
-    engine = tr_core_egl
-except ImportError :
-    print( 'ERROR >>> couldn\'t load headless-egl rendering backend' )
