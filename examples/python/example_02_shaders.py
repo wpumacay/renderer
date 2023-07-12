4d25ccd1405c6832f23749871ca6977ff8ff0e44
gregor <wpumacay@gmail.com>
2023-07-11 21:35:49 -0500

    (chore): update example of shader usage

--- examples/python/example_02_shaders.py
+++ examples/python/example_02_shaders.py
@@ -32,21 +32,7 @@ FRAG_SHADER_SRC = r"""
 
 def main():
     window = rdr.Window(WINDOW_WIDTH, WINDOW_HEIGHT, rdr.WindowBackend.TYPE_GLFW)
-
-    vert_shader = rdr.Shader("basic_2d_vert", rdr.ShaderType.VERTEX, VERT_SHADER_SRC)
-
-    frag_shader = rdr.Shader("basic_2d_frag", rdr.ShaderType.FRAGMENT, FRAG_SHADER_SRC)
-
-    if vert_shader.compiled:
-        print("Vertex Shader successfully compiled")
-
-    if frag_shader.compiled:
-        print("Fragment Shader successfully compiled")
-
-    program = rdr.Program("basic_2d")
-    program.AddShader(vert_shader)
-    program.AddShader(frag_shader)
-    program.LinkProgram()
+    program = rdr.Program("basic_2d", VERT_SHADER_SRC, FRAG_SHADER_SRC)
 
     if program.linked:
         print("Shader Program successfully linked")
