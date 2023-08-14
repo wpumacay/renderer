c9569e0ccfedd582e37e9963330a0f22eeac2883
gregor <wpumacay@gmail.com>
2023-08-14 12:00:13 -0500

    (tests): add c++ tests for window_config_t and window_t

--- tests/cpp/test_window.cpp
+++ tests/cpp/test_window.cpp
@@ -1,7 +1,36 @@
 #include <catch2/catch.hpp>
 
+#include <renderer/window/window_config_t.hpp>
+#include <renderer/window/window_t.hpp>
+
 TEST_CASE("Window class (window_t) type", "[window_t]") {
     SECTION("Constructor 1 - from config") {
-        ///
+        ::renderer::WindowConfig config;
+        config.backend = ::renderer::eWindowBackend::TYPE_GLFW;
+        config.width = 800;
+        config.height = 600;
+        config.title = "Test Window Constructors - from config";
+        config.gl_version_major = 4;
+        config.gl_version_minor = 3;
+
+        auto window = std::make_unique<::renderer::Window>(config);
+        REQUIRE(window->active());
+        REQUIRE(window->backend() == ::renderer::eWindowBackend::TYPE_GLFW);
+        REQUIRE(window->width() == 800);
+        REQUIRE(window->height() == 600);
+        REQUIRE(window->title() == "Test Window Constructors - from config");
+        REQUIRE(window->clear_color() == config.clear_color);
+    }
+
+    SECTION("Constructor 2 - from args") {
+        auto window = std::make_unique<::renderer::Window>(
+            800, 600, ::renderer::eWindowBackend::TYPE_GLFW);
+
+        REQUIRE(window->active());
+        REQUIRE(window->backend() == ::renderer::eWindowBackend::TYPE_GLFW);
+        REQUIRE(window->width() == 800);
+        REQUIRE(window->height() == 600);
+        REQUIRE(window->title() == "Application Window");
+        REQUIRE(window->clear_color() == Vec4(0.2F, 0.2F, 0.2F, 1.0F));
     }
 }
--- tests/cpp/test_window_config.cpp
+++ tests/cpp/test_window_config.cpp
@@ -1,7 +1,19 @@
 #include <catch2/catch.hpp>
 
+#include <renderer/window/window_config_t.hpp>
+
 TEST_CASE("WindowConfig class (window_config_t) type", "[window_config_t]") {
     SECTION("Default constructor") {
-        ///
+        auto config = ::renderer::WindowConfig();
+
+        REQUIRE(config.backend == ::renderer::DEFAULT_WINDOWING_BACKEND);
+        REQUIRE(config.width == ::renderer::DEFAULT_WINDOW_WIDTH);
+        REQUIRE(config.height == ::renderer::DEFAULT_WINDOW_HEIGHT);
+        REQUIRE(config.title == ::renderer::DEFAULT_WINDOW_TITLE);
+        REQUIRE(config.clear_color == Vec4(0.2F, 0.2F, 0.2F, 1.0F));
+        REQUIRE(config.gl_version_major ==
+                ::renderer::DEFAULT_OPENGL_MAJOR_VERSION);
+        REQUIRE(config.gl_version_minor ==
+                ::renderer::DEFAULT_OPENGL_MINOR_VERSION);
     }
 }
