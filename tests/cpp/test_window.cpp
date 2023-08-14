58ab1a4c66d08ec8552b35bb7203cfaad4e24d03
gregor <wpumacay@gmail.com>
2023-08-14 11:36:13 -0500

    (tests): adds basic layout for c++ tests

---
+++ tests/cpp/test_window.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("Window class (window_t) type", "[window_t]") {
+    SECTION("Constructor 1 - from config") {
+        ///
+    }
+}
---
+++ tests/cpp/test_window_config.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("WindowConfig class (window_config_t) type", "[window_config_t]") {
+    SECTION("Default constructor") {
+        ///
+    }
+}
