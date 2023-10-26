3bd7eed6f284dddb094235d6705479527b61eb78
gregor <wpumacay@gmail.com>
2023-10-26 15:01:47 -0500

    (tests): for VBO bindings

--- tests/python/buffers_test.py
+++ tests/python/buffers_test.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pytest
+from OpenGL import GL
 
 import renderer as rdr
 
@@ -8,133 +10,100 @@ def window():
     return rdr.Window(800, 600, rdr.WindowBackend.TYPE_GLFW)
 
 
-def test_buffer_element_ctor_default() -> None:
-    buff_elm = rdr.BufferElement()
-
-    assert buff_elm.name == "none"
-    assert buff_elm.type == rdr.ElementType.FLOAT_3
-    assert buff_elm.count == 3
-    assert buff_elm.size == 12
-    assert buff_elm.offset == 0
-    assert buff_elm.normalized is False
-
-
-def test_buffer_element_ctor_args() -> None:
-    elm_position = rdr.BufferElement("position", rdr.ElementType.FLOAT_3, False)
-    elm_normal = rdr.BufferElement("normal", rdr.ElementType.FLOAT_3, True)
-    elm_texcoord = rdr.BufferElement("texcoord", rdr.ElementType.FLOAT_2, False)
-
-    assert elm_position.name == "position"
-    assert elm_position.type == rdr.ElementType.FLOAT_3
-    assert elm_position.count == 3
-    assert elm_position.size == 12
-    assert elm_position.offset == 0
-    assert elm_position.normalized is False
-
-    assert elm_normal.name == "normal"
-    assert elm_normal.type == rdr.ElementType.FLOAT_3
-    assert elm_normal.count == 3
-    assert elm_normal.size == 12
-    assert elm_normal.offset == 0
-    assert elm_normal.normalized is True
-
-    assert elm_texcoord.name == "texcoord"
-    assert elm_texcoord.type == rdr.ElementType.FLOAT_2
-    assert elm_texcoord.count == 2
-    assert elm_texcoord.size == 8
-    assert elm_texcoord.offset == 0
-    assert elm_texcoord.normalized is False
-
-
 @pytest.fixture
-def layout():
+def layout_1():
     return rdr.BufferLayout(
         [
             ["position", rdr.ElementType.FLOAT_2, False],
             ["color", rdr.ElementType.FLOAT_3, False],
+        ]
+    )
+
+
+@pytest.fixture
+def layout_2():
+    return rdr.BufferLayout(
+        [
+            ["position", rdr.ElementType.FLOAT_3, False],
+            ["normal", rdr.ElementType.FLOAT_3, True],
             ["texcoord", rdr.ElementType.FLOAT_2, False],
         ]
     )
 
 
-def test_buffer_layout_ctor_default() -> None:
-    layout = rdr.BufferLayout()
-
-    assert layout.num_elements == 0
-    assert layout.stride == 0
-
-
-def test_buffer_layout_ctor_args(layout: rdr.BufferLayout) -> None:
-    assert layout.num_elements == 3
-    assert layout.stride == (2 * 4 + 3 * 4 + 2 * 4)
-
-
-def test_buffer_layout_add_element_method() -> None:
-    layout = rdr.BufferLayout()
-
-    layout.AddElement(
-        rdr.BufferElement("position", rdr.ElementType.FLOAT_3, False)
-    )
-    layout.AddElement(
-        rdr.BufferElement("normal", rdr.ElementType.FLOAT_3, True)
-    )
-    layout.AddElement(
-        rdr.BufferElement("texcoord", rdr.ElementType.FLOAT_2, False)
-    )
-
-    assert layout.num_elements == 3
-    assert layout.stride == (3 * 4 + 3 * 4 + 2 * 4)
+@pytest.fixture
+def data_1():
+    # fmt: off
+    buffer_data = np.array([
+        # |    pos    |    color    |#
+           -0.5, -0.5, 1.0, 0.0, 0.0,  # noqa: E131
+            0.5, -0.5, 0.0, 1.0, 0.0,  # noqa: E131
+            0.5,  0.5, 0.0, 0.0, 1.0,  # noqa: E131
+           -0.5, -0.5, 1.0, 0.0, 0.0,  # noqa: E131
+            0.5,  0.5, 0.0, 0.0, 1.0,  # noqa: E131
+           -0.5,  0.5, 1.0, 1.0, 1.0,  # noqa: E131
+    ], dtype=np.float32)
+    # fmt: on
+    return buffer_data
 
 
-def test_buffer_layout_get_element_method(layout: rdr.BufferLayout) -> None:
-    assert type(layout.GetElementByIndex(0)) is rdr.BufferElement
-    assert type(layout.GetElementByIndex(1)) is rdr.BufferElement
-    assert type(layout.GetElementByIndex(2)) is rdr.BufferElement
-
-    assert layout.GetElementByIndex(0).name == "position"
-    assert layout.GetElementByIndex(0).type == rdr.ElementType.FLOAT_2
-    assert layout.GetElementByIndex(0).count == 2
-    assert layout.GetElementByIndex(0).size == 2 * 4
-    assert layout.GetElementByIndex(0).offset == 0
-    assert layout.GetElementByIndex(0).normalized is False
-
-    assert layout.GetElementByIndex(1).name == "color"
-    assert layout.GetElementByIndex(1).type == rdr.ElementType.FLOAT_3
-    assert layout.GetElementByIndex(1).count == 3
-    assert layout.GetElementByIndex(1).size == 3 * 4
-    assert layout.GetElementByIndex(1).offset == layout[0].size
-    assert layout.GetElementByIndex(1).normalized is False
-
-    assert layout.GetElementByIndex(2).name == "texcoord"
-    assert layout.GetElementByIndex(2).type == rdr.ElementType.FLOAT_2
-    assert layout.GetElementByIndex(2).count == 2
-    assert layout.GetElementByIndex(2).size == 2 * 4
-    assert layout.GetElementByIndex(2).offset == layout[0].size + layout[1].size
-    assert layout.GetElementByIndex(2).normalized is False
+@pytest.fixture
+def data_2():
+    # fmt: off
+    buffer_data = np.array([
+        # |    position    |   normal   |-- texcoord --|#
+           -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  # noqa: E131
+            0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,  # noqa: E131
+            0.5,  0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,  # noqa: E131
+           -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  # noqa: E131
+            0.5,  0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,  # noqa: E131
+           -0.5,  0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0,  # noqa: E131
+    ], dtype=np.float32)
+    # fmt: on
+    return buffer_data
 
 
-def test_buffer_layout_getitem_operator(layout: rdr.BufferLayout) -> None:
-    assert type(layout[0]) is rdr.BufferElement
-    assert type(layout[1]) is rdr.BufferElement
-    assert type(layout[2]) is rdr.BufferElement
+def test_vertex_buffer_attrs():
+    assert hasattr(rdr.VertexBuffer, "layout")
+    assert hasattr(rdr.VertexBuffer, "size")
+    assert hasattr(rdr.VertexBuffer, "usage")
+    assert hasattr(rdr.VertexBuffer, "opengl_id")
 
-    assert layout[0].name == "position"
-    assert layout[0].type == rdr.ElementType.FLOAT_2
-    assert layout[0].count == 2
-    assert layout[0].size == 2 * 4
-    assert layout[0].offset == 0
-    assert layout[0].normalized is False
 
-    assert layout[1].name == "color"
-    assert layout[1].type == rdr.ElementType.FLOAT_3
-    assert layout[1].count == 3
-    assert layout[1].size == 3 * 4
-    assert layout[1].offset == layout[0].size
-    assert layout[1].normalized is False
+def test_vertex_buffer_ctor(
+    window: rdr.Window,
+    layout_1: rdr.BufferLayout,
+    layout_2: rdr.BufferLayout,
+    data_1: np.ndarray,
+    data_2: np.ndarray,
+) -> None:
+    vbo_1 = rdr.VertexBuffer(layout_1, rdr.BufferUsage.STATIC, data_1)
+    vbo_2 = rdr.VertexBuffer(layout_2, rdr.BufferUsage.STATIC, data_2)
+    assert vbo_1.opengl_id != 0
+    assert vbo_2.opengl_id != 0
 
-    assert layout[2].name == "texcoord"
-    assert layout[2].type == rdr.ElementType.FLOAT_2
-    assert layout[2].count == 2
-    assert layout[2].size == 2 * 4
-    assert layout[2].offset == layout[0].size + layout[1].size
-    assert layout[2].normalized is False
+    assert vbo_1.size == data_1.size * data_1.itemsize
+    assert vbo_2.size == data_2.size * data_2.itemsize
+
+    assert len(vbo_1.layout) == 2 and len(vbo_2.layout) == 3
+
+
+def test_vertex_buffer_bind_methods(
+    window: rdr.Window,
+    layout_1: rdr.BufferLayout,
+    layout_2: rdr.BufferLayout,
+    data_1: np.ndarray,
+    data_2: np.ndarray,
+) -> None:
+    vbo_1 = rdr.VertexBuffer(layout_1, rdr.BufferUsage.STATIC, data_1)
+    vbo_2 = rdr.VertexBuffer(layout_2, rdr.BufferUsage.STATIC, data_2)
+
+    vbo_1.Bind()
+    assert GL.glGetIntegerv(GL.GL_ARRAY_BUFFER_BINDING) == vbo_1.opengl_id
+    vbo_1.Unbind()
+    assert GL.glGetIntegerv(GL.GL_ARRAY_BUFFER_BINDING) == 0
+
+    vbo_2.Bind()
+    assert GL.glGetIntegerv(GL.GL_ARRAY_BUFFER_BINDING) == vbo_2.opengl_id
+    vbo_2.Unbind()
+    assert GL.glGetIntegerv(GL.GL_ARRAY_BUFFER_BINDING) == 0
