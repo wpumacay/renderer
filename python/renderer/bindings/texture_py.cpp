8a3490d6252e0cc1052d045fc3ce102116a56e0d
gregor <wpumacay@gmail.com>
2023-10-27 18:35:56 -0500

    (feat): adds .numpy() method to TexData bindings

--- python/renderer/bindings/texture_py.cpp
+++ python/renderer/bindings/texture_py.cpp
@@ -3,6 +3,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+#include <utils/logging.hpp>
+
 #include <renderer/core/texture_t.hpp>
 
 namespace py = pybind11;
@@ -41,10 +43,30 @@ auto bindings_texture(py::module& m) -> void {
             .def_property_readonly("image_path", &Class::image_path)
             .def_property_readonly("format", &Class::format)
             .def_property_readonly("storage", &Class::storage)
-            // TODO(wilbert): implement getter for data (as ref to numpy array)
+            .def("numpy",
+                 [](Class& self) -> py::array_t<uint8_t> {
+                     auto width = self.width();
+                     auto height = self.height();
+                     auto depth = self.channels();
+
+                     return py::array_t<uint8_t>({height, width, depth},
+                                                 {width * depth, depth, 1},
+                                                 self.data(), py::cast(self));
+                 })
             .def("__repr__",
                  [](const Class& self) -> py::str { return self.ToString(); });
     }
+
+    m.def("check_image_shape",
+          [](const py::array_t<uint8_t>& np_image) -> void {
+              auto info = np_image.request();
+              for (size_t i = 0; i < info.shape.size(); ++i) {
+                  LOG_CORE_INFO("shape-({0}) = {1}", i, info.shape[i]);
+              }
+              for (size_t i = 0; i < info.strides.size(); ++i) {
+                  LOG_CORE_INFO("stride-({0}) = {1}", i, info.strides[i]);
+              }
+          });
 }
 
 }  // namespace renderer
