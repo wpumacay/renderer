646140f4efb0ac45a5a4731f22ef6c82f13a845e
gregor <wpumacay@gmail.com>
2023-11-09 16:55:09 -0500

    (update): py-example with usage of ShaderManager

--- examples/python/example_06_managers.py
+++ examples/python/example_06_managers.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
+
+import os
 from typing import Optional
+
+import numpy as np
 
 import renderer as rdr
 
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+SHADERS_DIR = os.path.join(CURRENT_DIR, "..", "resources", "shaders")
+IMAGES_DIR = os.path.join(CURRENT_DIR, "..", "resources", "images")
+
 
 class Engine:
     def __init__(self):
         self.window: Optional[rdr.Window] = None
         self.input_manager: Optional[rdr.InputManager] = None
+        self.shader_manager: Optional[rdr.ShaderManager] = None
 
 
 g_engine = Engine()
@@ -21,6 +30,19 @@ def main() -> int:
         WINDOW_WIDTH, WINDOW_HEIGHT, rdr.WindowBackend.TYPE_GLFW
     )
     g_engine.input_manager = rdr.InputManager()
+    g_engine.shader_manager = rdr.ShaderManager()
+
+    if g_engine.window is None:
+        print("There was an error initializing the window")
+        return 1
+
+    if g_engine.input_manager is None:
+        print("There was an error initializing the input manager")
+        return 1
+
+    if g_engine.shader_manager is None:
+        print("There was an error initializing the shader manager")
+        return 1
 
     def keyboard_callback(key: int, action: int, _: int) -> None:
         if g_engine.input_manager is not None:
@@ -45,13 +67,19 @@ def main() -> int:
     g_engine.window.RegisterMouseMoveCallback(mouse_move_callback)
     g_engine.window.RegisterScrollCallback(scroll_callback)
 
-    if g_engine.window is None:
-        print("There was an error initializing the window")
-        return 1
+    program = g_engine.shader_manager.LoadProgram(
+        "basic2d",
+        os.path.join(SHADERS_DIR, "basic2d_vert.glsl"),
+        os.path.join(SHADERS_DIR, "basic2d_frag.glsl"),
+    )
 
-    if g_engine.input_manager is None:
-        print("There was an error initializing the input manager")
-        return 1
+    program.Bind()
+    color = np.array([0.2, 0.4, 0.8], dtype=np.float32)
+    radius = 0.1
+    program.SetFloat("u_time", 0.0)
+    program.SetFloat("u_radius", radius)
+    program.SetVec3("u_color", color)
+    program.Unbind()
 
     while g_engine.window.active:
         g_engine.window.Begin()
@@ -59,11 +87,34 @@ def main() -> int:
         if g_engine.input_manager.IsKeyDown(rdr.Keys.KEY_ESCAPE):
             g_engine.window.RequestClose()
 
-        print("Window: ")
-        print(g_engine.window)
-        print("InputManager: ")
-        print(g_engine.input_manager)
+        cursor_position = g_engine.input_manager.GetCursorPosition()
+        print(f"CursorPosition: ({cursor_position.x}, {cursor_position.y})")
+        print(
+            "MouseButtonLeft: {}".format(
+                g_engine.input_manager.IsMouseDown(rdr.Mouse.BUTTON_LEFT)
+            )
+        )
+        print(
+            "MouseButtonMiddle: {}".format(
+                g_engine.input_manager.IsMouseDown(rdr.Mouse.BUTTON_MIDDLE)
+            )
+        )
+        print(
+            "MouseButtonRight: {}".format(
+                g_engine.input_manager.IsMouseDown(rdr.Mouse.BUTTON_RIGHT)
+            )
+        )
 
+        # print("Window: ")
+        # print(g_engine.window)
+        # print("InputManager: ")
+        # print(g_engine.input_manager)
+        # print("ShaderManager: ")
+        # print(g_engine.shader_manager)
+
+        program.Bind()
+
+        program.Unbind()
         g_engine.window.End()
 
     return 0
