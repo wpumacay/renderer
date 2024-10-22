049ffecb5b3f8e62fd7719822c7e9910094381b5
wpumacay <wpumacay@gmail.com>
2024-10-21 21:35:35 -0500

    (Update): fix glad issue, update example

--- examples/cpp/example_02_shaders_opengl.cpp
+++ examples/cpp/example_02_shaders_opengl.cpp
@@ -1,34 +1,32 @@
 #include <chrono>
+#include <memory>
+
+#include <glad/gl.h>
 
 #include <renderer/engine/graphics/window_t.hpp>
 #include <renderer/backend/graphics/opengl/program_opengl_t.hpp>
-
-#include <glad/gl.h>
 
 #include <utils/logging.hpp>
 
 constexpr const char* VERT_SHADER_SRC = R"(
     #version 330 core
 
-    layout (location = 0) in vec2 pos;
-    layout (location = 1) in vec3 color;
-
-    out vec3 frag_color;
+    layout (location = 0) in vec3 pos;
 
     void main() {
-        gl_Position = vec4(pos, 0.0f, 1.0f);
-        frag_color = color;
+        gl_Position = vec4(pos, 1.0f);
     }
 )";
 
 constexpr const char* FRAG_SHADER_SRC = R"(
     #version 330 core
 
-    in vec3 frag_color;
     out vec4 output_color;
 
+    uniform float u_time;
+
     void main() {
-        output_color = vec4(frag_color, 1.0f);
+        output_color = vec4(abs(sin(u_time)), 0.0f, 0.0f, 1.0f);
     }
 )";
 
@@ -37,8 +35,15 @@ auto main() -> int {
     constexpr int WINDOW_HEIGHT = 768;
     constexpr auto WINDOW_API = ::renderer::eWindowBackend::TYPE_GLFW;
 
-    auto window = ::renderer::Window::CreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
-                                                   WINDOW_API);
+    renderer::WindowConfig win_config;
+    win_config.backend = WINDOW_API;
+    win_config.width = WINDOW_WIDTH;
+    win_config.height = WINDOW_HEIGHT;
+    win_config.title = "Example 02 - OpenGL Program";
+    win_config.gl_version_major = 3;
+    win_config.gl_version_minor = 3;
+
+    auto window = ::renderer::Window::CreateWindow(win_config);
     window->RegisterKeyboardCallback([&](int key, int, int) {
         if (key == ::renderer::keys::KEY_ESCAPE) {
             window->RequestClose();
@@ -55,11 +60,57 @@ auto main() -> int {
         LOG_ERROR("Shader Program couldn't be built");
     }
 
+    // clang-format off
+    // NOLINTNEXTLINE
+    float vertices[] = {
+        -0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+        -0.5,  0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.5,  0.5, 0.0,
+        -0.5,  0.5, 0.0,
+    };
+    // clang-format on
+
+    // NOLINTNEXTLINE
+    GLuint vao = 0, vbo = 0;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    // Prepare the layout for our data -----------------------------------------
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
+                 static_cast<const float*>(vertices), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    // -------------------------------------------------------------------------
+
+    auto t_start = std::chrono::steady_clock::now();
     while (window->active()) {
         window->Begin();
+        auto t_now = std::chrono::steady_clock::now();
+        auto t_current =
+            std::chrono::duration_cast<std::chrono::duration<float>>(t_now -
+                                                                     t_start)
+                .count();
+
+        program->Bind();
+        program->SetFloat("u_time", t_current);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        program->Unbind();
 
         window->End();
     }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
 
     return 0;
 }
